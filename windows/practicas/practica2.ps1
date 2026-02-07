2d5f2067b934c589eb28a033098f409dba3b7fb8
Jonathan <jonathanelian64@gmail.com>
2026-02-07 16:51:44 -0700

    feat: base de comandos para configurar DHCP server

---
+++ windows/practicas/practica2.ps1
@@ -0,0 +1,45 @@
+
+# Practica DHCP Server
+
+Function InstallDhcpServer() {
+
+    if (-not (CheckWindowsFeature "DHCP")) {
+        Write-WColor "Green" "Instalando DHCP Server..."
+        #Install-WindowsFeature DHCP -IncludeManagementTools
+    } else {
+        Write-WColor "Yellow" "DHCP Server ya esta instalado."  
+    }
+
+
+}
+
+Function ConfigureDhcpServer() {
+    Write-WColor "Green" "Configurando DHCP Server..."
+
+    New-NetIPAddress -IPAddress 10.0.0.3 -InterfaceAlias "Ethernet" -DefaultGateway 10.0.0.1 -AddressFamily IPv4 -PrefixLength 24
+    Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses 10.0.0.2
+    
+    #netsh dhcp add securitygroups
+    #Restart-Service dhcpserver
+    
+    Add-DhcpServerInDC -DnsName DHCP1.corp.contoso.com -IPAddress 10.0.0.3
+    Get-DhcpServerInDC
+    
+    Set-ItemProperty –Path registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ServerManager\Roles\12 –Name ConfigurationState –Value 2
+    
+    Set-DhcpServerv4DnsSetting -ComputerName "DHCP1.corp.contoso.com" -DynamicUpdates "Always" -DeleteDnsRRonLeaseExpiry $True
+    
+    Add-DhcpServerv4Scope -name "Corpnet" -StartRange 10.0.0.1 -EndRange 10.0.0.254 -SubnetMask 255.255.255.0 -State Active
+    #Add-DhcpServerv4ExclusionRange -ScopeID 10.0.0.0 -StartRange 10.0.0.1 -EndRange 10.0.0.15
+    #Set-DhcpServerv4OptionValue -OptionID 3 -Value 10.0.0.1 -ScopeID 10.0.0.0 -ComputerName DHCP1.corp.contoso.com
+    Set-DhcpServerv4OptionValue -DnsDomain corp.contoso.com -DnsServer 10.0.0.2
+}
+
+InstallDhcpServer
+
+
+#rem Configure scope Corpnet2
+#Add-DhcpServerv4Scope -name "Corpnet2" -StartRange 10.0.1.1 -EndRange 10.0.1.254 -SubnetMask 255.255.255.0 -State Active
+#Add-DhcpServerv4ExclusionRange -ScopeID 10.0.1.0 -StartRange 10.0.1.1 -EndRange 10.0.1.15
+#Set-DhcpServerv4OptionValue -OptionID 3 -Value 10.0.1.1 -ScopeID 10.0.1.0 -ComputerName DHCP1.corp.contoso.com
+
