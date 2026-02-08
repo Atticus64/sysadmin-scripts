78ddb004f44cf7c4e6df7b32b9cea1afdb40fbe3
Jonathan <jonathanelian64@gmail.com>
2026-02-08 14:46:29 -0700

    feat: validacion de instalacion

--- linux/practicas/practica2.sh
+++ linux/practicas/practica2.sh
@@ -3,10 +3,26 @@
 
 install_dhcp_server() {
 
-    check_dhcp=$(check_package_present "dhcp-server")
-    echo $check_dhcp
+    #check_dhcp=$(check_package_present "dhcp-server")
+    #echo $check_dhcp
+
+    if ! check_package_present "dhcp-server"; then
+        echo "No esta instalado"
+        #install_required_package "dhcp-server"
+        #if [[ $? -eq 0 ]]; then
+        #    echo "Paquete dhcp-server instalado correctamente"
+        #else 
+        #    echo "Error al instalar el paquete dhcp-server"
+        #    exit 1
+        #fi
+    else 
+        echo "El paquete dhcp-server ya está instalado"
+    fi
+
 }
 
-
+configurar_dhcp_server() {
+    #local nombreScope=$(input "Introduce el nombre del scope: ")
+}
 
 install_dhcp_server
