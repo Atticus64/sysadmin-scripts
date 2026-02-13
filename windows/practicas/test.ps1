e4b3d4eb9834f9e4934e13a2a9b01ba2c9648e3f
Jonathan <jonathanelian64@gmail.com>
2026-02-12 23:20:55 -0700

    feat: agregar funcion cowsay a powershell

---
+++ windows/practicas/test.ps1
@@ -0,0 +1,31 @@
+
+Function Get-StatusCmd($cmd) {
+
+    Try {
+        Invoke-Expression $cmd -OutVariable output 
+        return 0
+    } catch {
+        exit 1
+    }                                  
+}
+
+
+Function InstallCowsay {
+
+    if (Get-StatusCmd "Get-Command cowsay") {
+        Write-WColor Yellow "Cowsay ya esta instalado"
+    } else {
+        Write-WColor Yellow "Cowsay no esta instalado"
+
+        Invoke-WebRequest "https://raw.githubusercontent.com/kanej/Posh-Cowsay/refs/heads/master/cowsay.psm1" -OutFile "cowsay.psm1"
+    
+        Import-Module .\cowsay.psm1 -Force
+    }
+
+
+    Write-WColor Green "Cowsay instalado correctamente."
+
+    cowsay "El servidor esta andando"
+}
+
+
